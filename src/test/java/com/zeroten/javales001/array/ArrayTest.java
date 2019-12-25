e60ea3920ab778d00a7a22f927891496892c2808
qianyuxiang <410978619@qq.com>
2019-12-25 17:53:59 +0800

    Test 数组遍历

--- src/test/java/com/zeroten/javales001/array/ArrayTest.java
+++ src/test/java/com/zeroten/javales001/array/ArrayTest.java
@@ -2,11 +2,37 @@ package com.zeroten.javales001.array;
 
 import org.testng.annotations.Test;
 
+import javax.sound.midi.Soundbank;
+import java.sql.Array;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class ArrayTest {
 
     @Test
     public void testArrayDeclare(){
         int[] arr5=new int[] {12,13,12};
+
+    }
+
+    @Test
+    public void  testArrayBianli(){
+        Integer[] arr={1,2,3,4,5,6};
+
+        for(int index=0;index<arr.length;index++){
+            System.out.println(arr[index]);
+            System.out.printf("arr数组下标 %d 的值为 %d %n",index,arr[index]);
+            System.out.println(String.format("index:%d,value:%d",index,arr[index]));
+        }
+
+
+        for(int val:arr){
+            System.out.println(String.format("for each,value:%d",val));
+
+        }
+
+        Arrays.asList(arr).forEach(val-> System.out.printf("lambda value is %d %n",val));
+
 
     }
 }
