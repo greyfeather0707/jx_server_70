c4213142ecf3282255b1e540768f4e9ac4dd4780
qianyuxiang <410978619@qq.com>
2019-12-25 14:07:39 +0800

    add equal string

--- src/main/java/com/zeroten/common/util/CheckUitls.java
+++ src/main/java/com/zeroten/common/util/CheckUitls.java
@@ -1,6 +1,21 @@
 package com.zeroten.common.util;
 
 public class CheckUitls {
+
+    public static boolean equals(String str1,String str2){
+        if (str1.length()!=str2.length()){
+            return false;
+        }
+
+        for(int n=0;n<str1.length();n++){
+            if (str1.charAt(n)!=str2.charAt(n)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     public static boolean equals(Integer n1,Integer n2){
         return n1.intValue()==n2;
     }
--- src/test/java/com/zeroten/common/util/CheckUtilsTest.java
+++ src/test/java/com/zeroten/common/util/CheckUtilsTest.java
@@ -6,6 +6,21 @@ import org.testng.annotations.Test;
 public class CheckUtilsTest {
     @Test
     public void testEqualString(){
+        String str1="hello";
+        String str2="hello";
+        String str3="123jfe";
+        String str4="hell0";
+
+        Assert.assertEquals(CheckUitls.equals(str1,str2),true);
+        Assert.assertEquals(CheckUitls.equals(str1,str3),true);
+        Assert.assertEquals(CheckUitls.equals(str1,str4),true);
+        Assert.assertEquals(CheckUitls.equals(str3,str2),true);
+        Assert.assertEquals(CheckUitls.equals(str4,str2),true);
+
+    }
+
+    @Test
+    public void testEqualInteger(){
         Integer n1=234;
         Integer n2=234;
         Integer n3=34;
